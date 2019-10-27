7a4e88a60ce8d68c69f6d874cd3f38a745ebf4e1
yuanchang <759337828@qq.com>
2019-10-27 09:46:50 +0000

    index table style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-    def index
-      flash[:notice] = "早安！你好！"
-      flash[:alert] = "晚安！该睡了！"
-      flash[:warning] = "这是 warning 信息！"
-    end
+    flash[:notice] = "早安！你好！"
+    flash[:alert] = "晚安！该睡了！"
+    flash[:warning] = "这是 warning 信息！"
+    @groups = [
+        {
+            title: "test title one", description: "test description two"
+        },
+        {
+            title: "test title one", description: "test description two"
+        }
+    ]
   end
 end
