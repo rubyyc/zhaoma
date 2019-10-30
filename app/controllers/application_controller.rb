f46a6e98ce8e178a237705315220666fc2df7257
yuanchang <759337828@qq.com>
2019-10-30 09:41:49 +0000

    font-awesome-rails 图标字体 点击下拉菜单

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "暂无权限"
+      redirect_to root_path
+    end
+  end
 end
