74d62d3c8813bc9876113361c72a154e504ede5b
yuanchang <759337828@qq.com>
2019-10-31 05:43:52 +0000

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # get 'welcome/index'
-  # root 'welcome#index'
+  root 'welcome#index'
   resources :jobs do
     resources :resumes
   end
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
       resources :resumes
     end
   end
-  root 'jobs#index'
+  # root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
